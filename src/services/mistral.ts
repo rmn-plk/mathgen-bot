f67e98e7ad52c0f3a719701710cd83c4bd43c3f8
rmn-plk <rmn.polishchuk@gmail.com>
2025-04-05 18:40:42 +0300

    feat: add image recognition

--- src/services/mistral.ts
+++ src/services/mistral.ts
@@ -3,8 +3,8 @@ import { Mistral } from "@mistralai/mistralai";
 var mistral: Mistral;
 
 function initMistral() {
-    const apiKey = process.env.MISTRAL_API_KEY;
-    mistral = new Mistral({ apiKey: apiKey });
+  const apiKey = process.env.MISTRAL_API_KEY;
+  mistral = new Mistral({ apiKey: apiKey });
 }
 
 async function recognizeFile(fileUrl: string, isImage: boolean) {
@@ -19,12 +19,18 @@ async function recognizeFile(fileUrl: string, isImage: boolean) {
           type: "document_url",
           documentUrl: fileUrl,
         },
-    includeImageBase64: false,
+    includeImageBase64: true,
   });
-
-  const extractedText = ocrResponse.pages
-    .map((page) => page.markdown)
+  return ocrResponse.pages
+    .map((page) => {
+      let markdown = page.markdown;
+      page.images?.forEach((image) => {
+        const width = Math.abs(image.bottomRightX - image.topLeftX) / 2;
+        const height = Math.abs(image.topLeftY - image.bottomRightY) / 2;
+        markdown = markdown.replace(`(${image.id})`, `(${image?.imageBase64}){width=${width}px height=${height}px}`);
+      });
+      return markdown;
+    })
     .join("\n");
-  return extractedText;
 }
 export { initMistral, recognizeFile };
